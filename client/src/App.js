2777a2da605dd58da7481663c846aa83a896e2da
karapeoples <52808542+karapeoples@users.noreply.github.com>
2020-02-19 00:08:48 -0600

    MVP MET
    This commit:
    ~~Can Edit and Delete Movies
    ~~Finally got the last logic together
    
    Because:
    --{Functional MVP Code before attempting stretch or styling}

--- client/src/App.js
+++ client/src/App.js
@@ -3,26 +3,32 @@ import { Route } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
+import UpdateMovie from "./Movies/UpdateMovie";
 
 const App = () => {
-  const [savedList, setSavedList] = useState([]);
+	const [savedList, setSavedList] = useState([]);
 
-  const addToSavedList = movie => {
-    setSavedList([...savedList, movie]);
-  };
+	const addToSavedList = movie => {
+		setSavedList([...savedList, movie]);
+	};
 
-  return (
-    <>
-      <SavedList list={savedList} />
-      <Route exact path="/" component={MovieList} />
-      <Route
-        path="/movies/:id"
-        render={props => {
-          return <Movie {...props} addToSavedList={addToSavedList} />;
-        }}
+	return (
+		<>
+			<SavedList list={savedList} />
+			<Route exact path="/" component={MovieList} />
+			
+			<Route
+				path="/movies/:id"
+				render={props => {
+					return <Movie {...props} addToSavedList={addToSavedList} />;
+				}}
       />
-    </>
-  );
+			<Route path="/update-movie/:id" render={props =>
+				<UpdateMovie {...props} updateLists={addToSavedList} />
+			}
+      />
+		</>
+	);
 };
 
 export default App;
---
+++ client/src/Movies/UpdateMovie.js
@@ -0,0 +1,109 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+
+
+
+const initialItem = {
+  title: '',
+  director: '',
+  metascore: '',
+  stars: [],
+};
+
+const UpdateMovie = props => { 
+  const [movie, setMovie] = useState(initialItem);
+  const { id } = useParams();
+  
+  
+    useEffect(() => {
+      axios
+        .get(`http://localhost:5000/api/movies/${id}`)
+        .then(response => {
+          console.log(response)
+          setMovie(response.data)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }, [id])
+
+const changeHandler = ev => {
+    ev.persist();
+    let value = ev.target.value;
+    if (ev.target.name === 'metascore') {
+      value = parseInt(value, 10);
+    }
+
+    setMovie({
+      ...movie,
+      [ev.target.name]: value
+    });
+};
+  
+const handleStars = event => {
+  setMovie({
+    ...movie,
+    stars: [event.target.value],
+  })
+}
+
+  const handleSubmit = e => {
+    e.preventDefault();
+  axios
+  .put(`http://localhost:5000/api/movies/${id}`, movie)
+  .then(res => {
+  setMovie(initialItem)
+  props.history.push('/')
+})
+.catch(err => console.log(err))
+  };
+
+  return (
+    <div>
+      <h2>Update Movie</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="title"
+          onChange={changeHandler}
+          placeholder="Title"
+          value={movie.title}
+        />
+        <div/>
+
+        <input
+          type="text"
+          name="director"
+          onChange={changeHandler}
+          placeholder="Director"
+          value={movie.director}
+        />
+        <div/>
+
+        <input
+          type="number"
+          name="metascore"
+          onChange={changeHandler}
+          placeholder="Metascore"
+          value={movie.metascore}
+        />
+        <div/>
+        <input
+          type="text"
+          name="Stars"
+          onChange={handleStars}
+          placeholder="Stars"
+          value={movie.stars}
+        />
+        <div/>
+
+        
+
+        <button>Update</button>
+      </form>
+    </div>
+  );
+};
+
+export default UpdateMovie;
